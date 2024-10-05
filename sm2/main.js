da33746d264ef3db83542da9729f15b96b8cca3e
Зулькарнаев Джамиль Рамилевич <152314@mck-ktits.ru>
2024-10-05 08:20:25 +0300

    sm3

--- sm2/main.js
+++ sm2/main.js
@@ -1,18 +1,38 @@
-let name = prompt ("Как тебя зовут?");
+// let name = prompt ("Как тебя зовут?");
 
-        let age = prompt ("Сколько тебе лет?");
+// let age = prompt ("Сколько тебе лет?");
 
-        if (age < 20) {
-            alert ("Тебе меньше 20")
-        } 
-        else {
-            for (let i = 0; i < 5; i++) {
-                alert ("Привет!")
-            }
-        }
+// if (age < 20) {
+//     alert ("Тебе меньше 20")
+// } 
+// else {
+//     for (let i = 0; i < 5; i++) {
+//         alert ("Привет!")
+//     }
+// }
 
-        let want = confirm ("Хочешь учить Java?")
-        
-        if (want) {
-            alert ("Супер!")
-        }
+// let want = confirm ("Хочешь учить Java?")
+
+// if (want) {
+//     alert ("Супер!")
+// }
+
+// ---------------------
+
+// function sum (a, b) {
+//     return a + b
+// }
+
+// alert(sum(5, 1));
+
+// function sum (a, b) {
+//     alert(a + b);
+// }
+
+// sum(5, 1)
+
+function sum (a, b = 12) {
+    alert (a + b);
+}
+
+sum(5)
